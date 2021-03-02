d84d56cc00bf67d4bd05de422edd619f63d34e62
Dhruva <todhruva@mail.ru>
2021-03-02 11:30:45 +0300

    Begin work

---
+++ vue.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  publicPath: '/pub-wechart',
+  parallel: false,
+  "devServer": {
+    "port": 8282
+  },
+  	chainWebpack: config => {
+		const sRule = config.module.rule('stylus');
+		//console.log("--------------->",sRule);
+		sRule.uses.clear();
+		sRule
+			.test(/\.styl(us)?$/)
+			.oneOf('vue')
+	        	.resourceQuery(/vue/)
+				.use('stylus-loader')
+					.loader('stylus-loader')
+					.options( {stylusOptions: { sourceMap: false,preferPathResolver: 'webpack' }})
+					.end()
+			.end()		
+	}
+
+}
