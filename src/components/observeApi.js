66f4e00436c0f27d72c3934ab2a3fa5ff074bd3d
Dhruva <todhruva@mail.ru>
2021-03-09 15:30:22 +0300

    lint fix oserver

--- src/components/observeApi.js
+++ src/components/observeApi.js
@@ -1,11 +1,10 @@
-/* eslint-disable */		
+		
 export default {
 	methods: {
 
 		initObserver() {
 			let  config = { attributes: true }, vm = this;
 			// create the observer
-			var x = document.getElementById("sheet").parentElement
 			let elem = this.svg.parentElement
 			this.ds.width=elem.clientWidth
 			this.ds.height=elem.clientHeight
@@ -14,7 +13,6 @@ export default {
 				mutations.forEach(function(mutation) {					
 					if (mutation.type === "attributes") {
 						let width=elem.style.width, height=elem.style.height
-						let elemBox=elem.getBoundingClientRect();
 						width=elem.clientWidth
 						height=elem.clientHeight
 						vm.ds.height=(height)? height:vm.ds.height;
