be33460d4845c50576b43cc0963fc76356c7704b
Ubu20 <todhruva@mail.ru>
2025-08-05 11:40:03 +0300

    for publish gh-pages

---
+++ README.md
@@ -0,0 +1,22 @@
+# Vue 3 + Vite
+
+This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+
+Learn more about IDE Support for Vue in the [Vue Docs Scaling up Guide](https://vuejs.org/guide/scaling-up/tooling.html#ide-support).
+
+# https://vite.dev/guide/build.html#library-mode
+
+below link to template
+## https://github.com/vitejs/awesome-vite?tab=readme-ov-file#templates   
+
+
+below building tool
+## https://rollupjs.org/command-line-interface/#configuration-files
+
+CSS Pre-processors vite native support
+## https://vite.dev/guide/features.html#css-pre-processors
+
+## npm add -D vite-plugin-pug   && npm add -D stylus
+
+
+
---
+++ docs/assets/inputData-320439f7.js
@@ -0,0 +1,14 @@
+import"./modulepreload-polyfill-573c9133.js";import{_ as s,o as d,B as m,a as i,b as u,i as e,m as n,f as a,C as l,F as c,g as f}from"./oa-7c3b8bfd.js";import{H as g,N as b}from"./Header-16413217.js";/* empty css                        */import"./oaa-74838a69.js";const h={__name:"hLightData",setup(p){return d(async()=>{document.querySelectorAll("pre code").forEach(r=>{g.highlightElement(r)})}),(r,t)=>{const o=m("big");return u(),i("main",null,[e("article",null,[t[5]||(t[5]=e("h4",null,":points",-1)),e("p",null,[t[2]||(t[2]=n("Property value define data source for charting. This data source should be a nested array of the JSON object rows. Field ")),e("code",null,[a(o,null,{default:l(()=>t[0]||(t[0]=[n(" price:")])),_:1})]),t[3]||(t[3]=n(" should be a number otherwise error will rise. Field ")),e("code",null,[a(o,null,{default:l(()=>t[1]||(t[1]=[n(" tm:")])),_:1})]),t[4]||(t[4]=n(" has no strict types but for a timeseries chart should be fine if has ISO 8601 (YYYY-MM-DD) format. "))]),t[6]||(t[6]=e("pre",null,[e("code",{class:"language-js"},`
+  const data=[                                        
+    { name:"Chart1" , data:[             
+        {tm: '2020-08-14', price: 48.61}, 
+        {tm: '2020-08-15', price: 60.61}, 
+        {tm: '2020-08-16', price: 60.91}
+      ]},                                    
+     { name:"Chart2" , data:[             
+        {tm: '2020-08-14', price: 18.61}, 
+        {tm: '2020-08-15', price: 20.61}, 
+        {tm: '2020-08-16', price: 19.91}
+      ]},                                      
+  ]                                         
+`)],-1)),t[7]||(t[7]=e("pre",null,[e("code",{class:"language-js"},' < WeDoChart :points="data"/> ')],-1))])])}}},_=s(h,[["__scopeId","data-v-0d791d11"]]),v={__name:"InputData",setup(p){return(r,t)=>(u(),i(c,null,[e("header",null,[a(b,{id:"mynav",active:"2"})]),e("body",null,[e("main",null,[e("article",null,[a(_)])])])],64))}},y=s(v,[["__scopeId","data-v-a967497b"]]);f(y).mount("#app");
